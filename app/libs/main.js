9ebab7f5703df0d9f18c1a225eb9ac6f0444f286
Evgenij Ivanov <evgeney.ivanov@gmail.com>
2017-04-03 13:30:34 +0300

    fix libs and main .js

--- app/libs/main.js
+++ app/libs/main.js
@@ -2,17 +2,17 @@
  * library
  */
 //= jquery/dist/jquery.min.js
-// magnific-popup/dist/jquery.magnific-popup.min.js
-//= fancybox/source/jquery.fancybox.pack.js
-//= jquery-mousewheel/jquery.mousewheel.js
-//= waypoints/lib/jquery.waypoints.js
-//= owl-carousel/owl-carousel/owl.carousel.js
-//= jquery.scrollTo/jquery.scrollTo.js
+//  magnific-popup/dist/jquery.magnific-popup.min.js
+//  fancybox/source/jquery.fancybox.pack.js
+//  jquery-mousewheel/jquery.mousewheel.js
+//  waypoints/lib/jquery.waypoints.js
+//  owl-carousel/owl-carousel/owl.carousel.js
+//  jquery.scrollTo/jquery.scrollTo.js
 //= countdown/dest/jquery.countdown.js
-//= landing-nav/navigation.js
+//  landing-nav/navigation.js
 
 /*
  * Custom
  */
-//= custom.js
+//  custom.js
 //= common.js
